64f8160eb0e83d218676c7624e7d29e287e9d215
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-09-08 10:09:20 -0600

    feat(main): add sum operation

---
+++ main.py
@@ -0,0 +1,25 @@
+def main():
+    print("Calculadora")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    value = int(input("Selecciona la operacion a realizar: "))
+    print("La opción que selecciono es: " + str(value))
+
+    if value == 1:
+        resultado = menu_sumar()
+        print("El resultado de la suma es: " + str(resultado))
+
+def menu_sumar():
+    print("Menu sumar")
+    num = int(input("Ingrese el primer sumando: "))
+    num2 = int(input("Ingrese el segundo sumando: "))
+    return sumar(num, num2)
+
+def sumar(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    main()
